2cc9f4c2147abff4d677be8f8764e48c44782854
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 13:22:16 +0330

    feat: add user cart relation

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -19,11 +19,11 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    public function cart(): HasOne
+    {
+        return $this->hasOne(Cart::class);
+    }
+
     protected function casts(): array
     {
         return [
