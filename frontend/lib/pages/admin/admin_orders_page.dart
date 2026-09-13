179d05e90cdb56c3808d69cd376484889038f758
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 17:21:57 +0330

    fix: import shared admin provider

--- frontend/lib/pages/admin/admin_orders_page.dart
+++ frontend/lib/pages/admin/admin_orders_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../data/repositories/admin_repository.dart';
 import '../../models/order_model.dart';
+import '../../providers/admin_provider.dart';
 
 class AdminOrdersPage extends ConsumerStatefulWidget {
   const AdminOrdersPage({super.key});
