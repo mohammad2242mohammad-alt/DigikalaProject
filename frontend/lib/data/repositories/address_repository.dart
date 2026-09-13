d9f39b43f26d96c4295c766bcfa11f415b7342d6
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 16:50:11 +0330

    feat: add address repository

---
+++ frontend/lib/data/repositories/address_repository.dart
@@ -0,0 +1,70 @@
+import '../../core/network/api_client.dart';
+import '../../models/address_model.dart';
+
+class AddressRepository {
+  AddressRepository(this._apiClient);
+
+  final ApiClient _apiClient;
+
+  Future<List<AddressModel>> getAddresses() async {
+    final response = await _apiClient.get('/addresses');
+    if (response is! Map<String, dynamic> || response['data'] is! List) {
+      throw const FormatException('Invalid addresses response');
+    }
+    return (response['data'] as List)
+        .whereType<Map<String, dynamic>>()
+        .map(AddressModel.fromJson)
+        .toList();
+  }
+
+  Future<AddressModel> create({
+    String? title,
+    required String recipientName,
+    required String phone,
+    required String province,
+    required String city,
+    required String address,
+    required String postalCode,
+    bool isDefault = false,
+  }) async {
+    final response = await _apiClient.post('/addresses', body: {
+      if (title != null) 'title': title,
+      'recipient_name': recipientName,
+      'phone': phone,
+      'province': province,
+      'city': city,
+      'address': address,
+      'postal_code': postalCode,
+      'is_default': isDefault,
+    });
+    return AddressModel.fromJson(response['data'] as Map<String, dynamic>);
+  }
+
+  Future<AddressModel> update({
+    required int id,
+    String? title,
+    required String recipientName,
+    required String phone,
+    required String province,
+    required String city,
+    required String address,
+    required String postalCode,
+    bool isDefault = false,
+  }) async {
+    final response = await _apiClient.patch('/addresses/$id', body: {
+      if (title != null) 'title': title,
+      'recipient_name': recipientName,
+      'phone': phone,
+      'province': province,
+      'city': city,
+      'address': address,
+      'postal_code': postalCode,
+      'is_default': isDefault,
+    });
+    return AddressModel.fromJson(response['data'] as Map<String, dynamic>);
+  }
+
+  Future<void> delete(int id) async {
+    await _apiClient.delete('/addresses/$id');
+  }
+}
