3f0475915acd540d29de5b0ca360c7331dcea8ea
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 16:55:41 +0330

    feat: complete Flutter cart mutations

--- frontend/lib/data/repositories/cart_repository.dart
+++ frontend/lib/data/repositories/cart_repository.dart
@@ -14,6 +14,14 @@ class CartRepository {
         'quantity': quantity,
       }));
 
+  Future<CartModel> updateItem({required int itemId, required int quantity}) async =>
+      _parse(await _apiClient.patch('/cart/items/$itemId', body: {
+        'quantity': quantity,
+      }));
+
+  Future<CartModel> removeItem(int itemId) async =>
+      _parse(await _apiClient.delete('/cart/items/$itemId'));
+
   Future<CartModel> clear() async => _parse(await _apiClient.delete('/cart'));
 
   CartModel _parse(dynamic response) {
