fb61d234a99fef162542f817e7cfa1cc732fa028
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 17:21:03 +0330

    fix: centralize admin repository provider

---
+++ frontend/lib/providers/admin_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../core/network/api_client.dart';
+import '../data/repositories/admin_repository.dart';
+
+final adminRepositoryProvider = Provider<AdminRepository>(
+  (ref) => AdminRepository(ref.watch(apiClientProvider)),
+);
