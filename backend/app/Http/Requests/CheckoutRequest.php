de68f81853a0cbb55e3aeb9f750b5611a05b06ab
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 16:49:25 +0330

    feat: add checkout request

---
+++ backend/app/Http/Requests/CheckoutRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CheckoutRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'address_id' => [
+                'required',
+                'integer',
+                Rule::exists('addresses', 'id')->where(
+                    fn ($query) => $query->where('user_id', $this->user()->id),
+                ),
+            ],
+        ];
+    }
+}
