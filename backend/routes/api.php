6cf6020ef648383d766c1750f574060fad26d92a
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 12:50:31 +0330

    feat: expose category API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
 Route::apiResource('products', ProductController::class);
