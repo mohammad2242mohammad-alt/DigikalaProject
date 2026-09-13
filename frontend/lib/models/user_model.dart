b82dbe38c1ff7bbcdf5e0c2ca45cffabbd5118b9
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 14:04:10 +0330

    feat: add user model

---
+++ frontend/lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final int id;
+  final String name;
+  final String email;
+
+  const UserModel({required this.id, required this.name, required this.email});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: (json['id'] as num).toInt(),
+        name: json['name']?.toString() ?? '',
+        email: json['email']?.toString() ?? '',
+      );
+}
