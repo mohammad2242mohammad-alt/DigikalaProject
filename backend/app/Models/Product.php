252512fad576246781e0d62a0a73e731b40e02c6
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 13:22:25 +0330

    feat: add product cart relation

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -32,5 +33,10 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function cartItems(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
     }
 }
