201d55f109ea4d51f578a23208417c412d02a339
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 16:50:04 +0330

    feat: add address model

---
+++ frontend/lib/models/address_model.dart
@@ -0,0 +1,35 @@
+class AddressModel {
+  final int id;
+  final String? title;
+  final String recipientName;
+  final String phone;
+  final String province;
+  final String city;
+  final String address;
+  final String postalCode;
+  final bool isDefault;
+
+  const AddressModel({
+    required this.id,
+    this.title,
+    required this.recipientName,
+    required this.phone,
+    required this.province,
+    required this.city,
+    required this.address,
+    required this.postalCode,
+    required this.isDefault,
+  });
+
+  factory AddressModel.fromJson(Map<String, dynamic> json) => AddressModel(
+        id: (json['id'] as num).toInt(),
+        title: json['title']?.toString(),
+        recipientName: json['recipient_name']?.toString() ?? '',
+        phone: json['phone']?.toString() ?? '',
+        province: json['province']?.toString() ?? '',
+        city: json['city']?.toString() ?? '',
+        address: json['address']?.toString() ?? '',
+        postalCode: json['postal_code']?.toString() ?? '',
+        isDefault: json['is_default'] == true || json['is_default'] == 1,
+      );
+}
