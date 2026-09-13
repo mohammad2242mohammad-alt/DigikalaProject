e392f31cb3d46a2af149c98751bfcaa746002628
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 14:03:41 +0330

    refactor: initialize Riverpod app scope

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,41 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'pages/home/home_page.dart';
 
-
 void main() {
-  runApp(const DigikalaApp());
+  runApp(const ProviderScope(child: DigikalaApp()));
 }
 
-
 class DigikalaApp extends StatelessWidget {
-
   const DigikalaApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-
       title: 'Digikala',
-
-
       theme: ThemeData(
-
         useMaterial3: true,
-
         fontFamily: 'Arial',
-
       ),
-
-
       home: const HomePage(),
-
     );
-
   }
-
 }
