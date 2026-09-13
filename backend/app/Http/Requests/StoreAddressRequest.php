fd54582434d2de3939df21b12f3168470dea158c
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-13 16:45:27 +0330

    feat: add address validation

---
+++ backend/app/Http/Requests/StoreAddressRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAddressRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['nullable', 'string', 'max:100'],
+            'recipient_name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:30'],
+            'province' => ['required', 'string', 'max:100'],
+            'city' => ['required', 'string', 'max:100'],
+            'address' => ['required', 'string', 'max:2000'],
+            'postal_code' => ['required', 'string', 'max:20'],
+            'is_default' => ['sometimes', 'boolean'],
+        ];
+    }
+}
