92a8ddae6a0c79f615a5c5a52c2a15fd36fc7911
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-14 12:08:27 +0330

    refactor: preserve provider api compatibility

--- frontend/lib/providers/product_provider.dart
+++ frontend/lib/providers/product_provider.dart
@@ -4,6 +4,8 @@ import '../data/repositories/product_repository.dart';
 import '../models/product_model.dart';
 import 'api_provider.dart';
 
+export 'api_provider.dart';
+
 final productRepositoryProvider = Provider<ProductRepository>(
   (ref) => ProductRepository(ref.watch(apiClientProvider)),
 );
