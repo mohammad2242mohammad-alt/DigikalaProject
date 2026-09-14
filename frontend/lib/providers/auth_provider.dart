9ceacf64fb9a4246a99c5a1ea8b7a9dd09a95f2b
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-14 12:08:18 +0330

    refactor: use centralized api provider

--- frontend/lib/providers/auth_provider.dart
+++ frontend/lib/providers/auth_provider.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../data/repositories/auth_repository.dart';
 import '../models/user_model.dart';
-import 'product_provider.dart';
+import 'api_provider.dart';
 
 final authRepositoryProvider = Provider<AuthRepository>(
   (ref) => AuthRepository(ref.watch(apiClientProvider)),
