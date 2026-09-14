d456414dc04ed16cf7d88c932670ba07052bae4d
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-14 12:29:27 +0330

    refactor: move product search loading to provider

--- frontend/lib/providers/product_provider.dart
+++ frontend/lib/providers/product_provider.dart
@@ -17,3 +17,55 @@ final productsProvider = FutureProvider<List<Product>>((ref) {
 final productDetailProvider = FutureProvider.family<Product, int>((ref, id) {
   return ref.watch(productRepositoryProvider).getProduct(id);
 });
+
+class ProductSearchQuery {
+  const ProductSearchQuery({
+    this.search = '',
+    this.categoryId,
+    this.minPrice,
+    this.maxPrice,
+    this.sort = 'latest',
+    this.perPage = 50,
+  });
+
+  final String search;
+  final int? categoryId;
+  final double? minPrice;
+  final double? maxPrice;
+  final String sort;
+  final int perPage;
+
+  @override
+  bool operator ==(Object other) {
+    return other is ProductSearchQuery &&
+        other.search == search &&
+        other.categoryId == categoryId &&
+        other.minPrice == minPrice &&
+        other.maxPrice == maxPrice &&
+        other.sort == sort &&
+        other.perPage == perPage;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        search,
+        categoryId,
+        minPrice,
+        maxPrice,
+        sort,
+        perPage,
+      );
+}
+
+final productSearchProvider = FutureProvider.family<List<Product>, ProductSearchQuery>(
+  (ref, query) {
+    return ref.watch(productRepositoryProvider).getProducts(
+          search: query.search,
+          categoryId: query.categoryId,
+          minPrice: query.minPrice,
+          maxPrice: query.maxPrice,
+          sort: query.sort,
+          perPage: query.perPage,
+        );
+  },
+);
