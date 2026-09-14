6af98e0f4855f080667d4b43c03b1386d8eb5963
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-14 12:07:57 +0330

    refactor: use centralized api provider

--- frontend/lib/providers/address_provider.dart
+++ frontend/lib/providers/address_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../data/repositories/address_repository.dart';
 import '../models/address_model.dart';
-import 'product_provider.dart';
+import 'api_provider.dart';
 
 final addressRepositoryProvider = Provider<AddressRepository>(
   (ref) => AddressRepository(ref.watch(apiClientProvider)),
