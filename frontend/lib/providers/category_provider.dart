eb41216204cffd83d9bcba28baa8d4a0a3cf0def
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-14 12:08:01 +0330

    refactor: use centralized api provider

--- frontend/lib/providers/category_provider.dart
+++ frontend/lib/providers/category_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../data/repositories/category_repository.dart';
 import '../models/category_model.dart';
-import 'product_provider.dart';
+import 'api_provider.dart';
 
 final categoryRepositoryProvider = Provider<CategoryRepository>(
   (ref) => CategoryRepository(ref.watch(apiClientProvider)),
