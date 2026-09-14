6223b231256b98962f5ca0112d27e6f0134f2710
mohammad2242mohammad-alt <mohammad2242mohammad@gmail.com>
2026-09-14 12:07:47 +0330

    refactor: use centralized api provider

--- frontend/lib/providers/product_provider.dart
+++ frontend/lib/providers/product_provider.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../core/network/api_client.dart';
 import '../data/repositories/product_repository.dart';
 import '../models/product_model.dart';
-
-final apiClientProvider = Provider<ApiClient>((ref) => ApiClient());
+import 'api_provider.dart';
 
 final productRepositoryProvider = Provider<ProductRepository>(
   (ref) => ProductRepository(ref.watch(apiClientProvider)),
